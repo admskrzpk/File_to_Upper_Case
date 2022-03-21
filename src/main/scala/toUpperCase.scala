14046c784b1a4b2d6618b72169db2cb65a94d30b
a.skrzypek <a.skrzypek@adform.com>
2022-03-21 13:41:06 +0100

    with zipWithIndex & foreach

--- src/main/scala/toUpperCase.scala
+++ src/main/scala/toUpperCase.scala
@@ -5,8 +5,8 @@ object toUpperCase extends App {
     val line = Source
       .fromFile(args(1))
       .getLines
-      .flatMap(c => c.split("\\W+"))
+      .flatMap(_.split("\\W+"))
       .zipWithIndex
-      .map(w => if (w._2 % n ==0) {w._1.toUpperCase} else w._1 )
-.foreach(println)
+      .map(w => if (w._2 % n == 0) {w._1.toUpperCase} else w._1 )
+      .foreach(println)
 }
